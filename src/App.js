5ba6964cd85cbde0e5ce5013a1dc5d1153c8e04c
coolgourav147 <coolgourav147@gmail.com>
2020-06-15 23:32:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gaurav Sharma - 13
+          Gaurav Sharma - 14
         </p>
         <a
           className="App-link"
